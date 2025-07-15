99cf48c365434d0da6b6f5f7442cceb3236bbf70
tantd21 <ta090707@gmail.com>
2025-07-15 11:49:36 +0700

    test

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,18 +1,9 @@
-// firebase.js
 const admin = require("firebase-admin");
 
-// Parse JSON từ biến môi trường
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://trello-app-2ebad.firebaseio.com",
-});
-
-module.exports = { db: admin.database() };
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
